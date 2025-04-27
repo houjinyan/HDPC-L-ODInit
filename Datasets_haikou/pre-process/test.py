02b5de98cd4a933174f4cb03c81f3299b36b68db
houjinyan <88902696+houjinyan@users.noreply.github.com>
2025-04-27 16:09:22 +0800

    Delete Datasets_haikou directory

--- Datasets_haikou/pre-process/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-if __name__ == "__main__":
-    haikou_1=pd.DataFrame(pd.read_csv('../海口网约车/dwv_order_make_haikou_1.txt', sep='\t'))
-    print(haikou_1['dwv_order_make_haikou_1.arrive_time'])
